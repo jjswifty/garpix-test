0dfe92746913ded84c155b2b9c50c8de3a161ff0
jjswift <supercheater2018@yandex.ru>
2020-10-04 23:40:07 +0300

    Redirect to 404

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react'
 import s from'./App.module.sass'
-import { BrowserRouter, Route } from 'react-router-dom'
+import { BrowserRouter, Redirect, Route, Switch } from 'react-router-dom'
 import { Header } from './components/Header/Header'
 import { Provider } from 'react-redux'
 import { store } from './components/redux/store'
 import { PartnersPage } from './components/PartnersPage/PartnersPage'
 import { Footer } from './components/Footer/Footer'
+import { NotFound } from './components/NotFound/NotFound'
 
 export const App = () => {
     return (
@@ -14,7 +15,11 @@ export const App = () => {
                 <Header />
                 <div className={s.appWrapper}>
                     <div className={s.appWrapperContent}>
-                        <Route path="/Партнеры" render={() => <PartnersPage />}/>
+                    <Switch>
+                        <Route exact path='/Партнеры' component={PartnersPage} />
+                        <Route path="/404" component={NotFound} />
+                        <Redirect to="/404" />
+                    </Switch>
                     </div>
                 </div>
                 <Footer />
---
+++ src/components/NotFound/NotFound.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import s from './NotFound.module.sass'
+
+export const NotFound = () => (
+    <div className={s.container}>
+        <h1>Упс.. Кажется здесь ничего нет. Может быть открыть страницу "Партнёры"?</h1>
+    </div>
+)
